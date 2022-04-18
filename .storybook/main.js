69f95a28a0f9da7d830eba57d6650db3a2085f24
Kai <jazztyy@gmail.com>
2022-04-18 14:46:02 +0800

    init

---
+++ .storybook/main.js
@@ -0,0 +1,13 @@
+const { mergeConfig } = require('vite');
+
+module.exports = {
+  "stories": ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  "addons": ["@storybook/addon-links", "@storybook/addon-essentials"],
+  "framework": "@storybook/vue3",
+  "core": {
+    "builder": "@storybook/builder-vite"
+  },
+  async viteFinal(config) {
+    return mergeConfig(config);
+  },
+};
