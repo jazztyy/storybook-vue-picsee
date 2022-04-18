ffed19db912db7b0cc1d037a86823dabfa6ec895
Kai <jazztyy@gmail.com>
2022-04-18 15:47:29 +0800

    Storybook deployment with GitHub action

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,7 +2,22 @@ const { mergeConfig } = require('vite');
 
 module.exports = {
   "stories": ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  "addons": ["@storybook/addon-links", "@storybook/addon-essentials"],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    '@storybook/addon-storysource',
+    '@storybook/addon-knobs',
+  {
+    name: '@storybook/addon-postcss',
+    options: {
+      cssLoaderOptions: {
+        importLoaders: 1,
+      },
+      postcssLoaderOptions: {
+        implementation: require('postcss'),
+      },
+    },
+  },],
   "framework": "@storybook/vue3",
   "core": {
     "builder": "@storybook/builder-vite"
